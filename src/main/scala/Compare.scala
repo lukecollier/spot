79cc3adefbe43baa3b49cd51123bd772397d2760
Luke Collier <leccollier@gmail.com>
2019-11-15 14:06:08 +0000

    now can check responses after making http request

--- src/main/scala/Compare.scala
+++ src/main/scala/Compare.scala
@@ -6,8 +6,9 @@ import cats.{Eq, Monad}
 import spot.internals.DifferenceValidatorNec.validate
 import sttp.client._
 import sttp.model.{Header, Uri}
+import cats.data._
 
-import scala.language.higherKinds
+import spot.internals.Difference
 
 object Compare {
 
@@ -35,12 +36,11 @@ object Compare {
   }
 
   val headerComparator: Comparator[Vector[Header]] = (orig, alt) => {
-      if (validate(orig, alt).isValid) {
-        Same()
-      } else {
-        Different()
-      }
+    validate(orig, alt).toEither match {
+      case Right(_) => Same
+      case Left(nec) => Different(nec)
     }
+  }
 
   private[compare] def compare[A](a: Res[A], b: Res[A])(implicit co: Comparator[A]) = {
     (a.body, b.body) match {
@@ -53,45 +53,70 @@ object Compare {
     }
   }
 
-  def buildAnalysis[A, WS[_]](host: Uri, hosts: (Uri, Uri), reqs: List[PartReq[A]])(
+  // needs to convert to event driven so we can continually run this
+  def runAnalysis[A, WS[_]](host: Uri, hosts: (Uri, Uri), reqs: List[PartReq[A]])(
     implicit co: Comparator[A], b: SttpBackend[IO, Nothing, WS]) = {
       reqs.map { partialRequest =>
         request[A, WS](host, hosts, partialRequest).map {
           case Requests(candidate, primary, secondary) =>
             val primarySecondary = compare(primary, secondary)
             val canidatePrimary = compare(candidate, primary)
-            // compare primary secondary and candidate primary and see if the differences are the same
-            Same()
+            val bodyRes = (primarySecondary._1, canidatePrimary._1) match {
+              case (Different(primaryErr), Different(candidateErr)) => 
+                println(primaryErr, candidateErr)
+                if (primaryErr == candidateErr) {
+                  Same
+                } else {
+                  Different(candidateErr)
+                }
+              case (Different(chain), Same) => Same
+              case (Same, Different(chain)) => Different(chain)
+              case (Same, Same) => Same
+            }
+            val headerRes = (primarySecondary._2, canidatePrimary._2) match {
+              case (Different(primaryErr), Different(candidateErr)) => ??? // if same return same
+              case (Different(chain), Same) => Same
+              case (Same, Different(chain)) => Different(chain)
+              case (Same, Same) => Same
+            }
+            (bodyRes, headerRes)
         }
       }
   }
 
 }
 
-sealed trait Comparison[A] // should implement Semigroup
-final case class Different[A]() extends Comparison[A]
-final case class Same[A]() extends Comparison[A]
+sealed trait Comparison
+final case class Different[A](diffs: NonEmptyChain[Difference[A]]) extends Comparison
+final case object Same extends Comparison
 
 trait Comparator[A] { 
-  def compare(orig: A, alt: A): Comparison[A]
+  def compare(orig: A, alt: A): Comparison
 }
 
 object Comparator {
   implicit val listString: Comparator[List[String]] = (orig: List[String], alt: List[String]) => {
-    if (validate(orig, alt).isValid) {
-      Same()
-    } else {
-      Different()
+    validate(orig, alt).toEither match {
+      case Right(_) => Same
+      case Left(nec) => Different(nec)
+    }
+  }
+
+  implicit val string: Comparator[String] = (orig: String, alt: String) => {
+    val origList: List[Char] = orig.toList
+    val altList: List[Char] = alt.toList
+    validate(origList, altList).toEither match {
+      case Right(_) => Same
+      case Left(nec) => Different(nec)
     }
   }
 
   implicit val byteArray: Comparator[Array[Byte]] = (orig: Array[Byte], alt: Array[Byte]) => {
     val origList: List[Byte] = orig.toList
     val altList: List[Byte] = alt.toList
-    if (validate(origList, altList).isValid) {
-      Same()
-    } else {
-      Different()
+    validate(origList, altList).toEither match {
+      case Right(_) => Same
+      case Left(nec) => Different(nec)
     }
   }
 }
--- src/test/scala/CompareTest.scala
+++ src/test/scala/CompareTest.scala
@@ -6,9 +6,12 @@ import sttp.client.asynchttpclient.cats.AsyncHttpClientCatsBackend
 import sttp.client.testing.SttpBackendStub
 import sttp.model.{Header, StatusCode}
 import utest._
-
+import cats.implicits._
+import cats.data._
 import scala.concurrent.ExecutionContext
-import spot.compare.Compare.compare
+
+import spot.compare.Compare.{compare, runAnalysis}
+import spot.internals.{Difference, Additional, Removed, Ordering}
 
 
 // todo fix
@@ -16,9 +19,10 @@ import spot.compare.Compare.compare
 object CompareTest extends TestSuite{
   val tests = Tests {
     test("can make requests") {
-      val candidateResponse: Response[Either[String, String]] = Response(Right("candidate"), StatusCode.Ok, "OK")
-      val firstResponse: Response[Either[String, String]] = Response(Right("first"), StatusCode.Ok, "OK")
-      val secondResponse: Response[Either[String, String]] = Response(Right("second"), StatusCode.Ok, "OK")
+      type TestResponse = Response[Either[String, Array[Byte]]]
+      val candidateResponse: TestResponse = Response(Right(Array(1)), StatusCode.Ok, "OK")
+      val firstResponse: TestResponse = Response(Right(Array(2)), StatusCode.Ok, "OK")
+      val secondResponse: TestResponse = Response(Right(Array(3)), StatusCode.Ok, "OK")
 
       implicit val contextShift: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
       AsyncHttpClientCatsBackend[cats.effect.IO]().map {
@@ -45,32 +49,129 @@ object CompareTest extends TestSuite{
           Response(Right(Array(0, 1, 2)), StatusCode.Ok, "OK")
 
         val res = compare[Array[Byte]](first, second)(Comparator.byteArray)
-        val exp = (Same(), Same())
+        val exp = (Same, Same)
         assert(res == exp)
 
       }
       test("body can be different") {
+        val removed: Byte = 2
         val first: Response[Either[String, Array[Byte]]] =
-          Response(Right(Array(0, 1, 2)), StatusCode.Ok, "OK")
+          Response(Right(Array(0, 1, removed)), StatusCode.Ok, "OK")
         val second: Response[Either[String, Array[Byte]]] =
           Response(Right(Array(0, 1)), StatusCode.Ok, "OK")
 
         val res = compare[Array[Byte]](first, second)(Comparator.byteArray)
-        val exp = (Different(), Same())
+        val exp = (Different(NonEmptyChain(Removed(removed))), Same)
         assert(res == exp)
       }
       test("header can be different") {
         val base: Response[Either[String, Array[Byte]]] =
           Response(Right(Array(1, 2, 3)), StatusCode.Ok, "OK")
 
-        val first = base.copy(headers = Seq(Header("X-experiment-seed", "123")))
-        val second = base.copy(headers = Seq(Header("X-experiment-seed", "124")))
+        val firstHeader = Header("X-experiment-seed", "123")
+        val secondHeader = Header("X-experiment-seed", "124")
+        val first = base.copy(headers = Seq(firstHeader))
+        val second = base.copy(headers = Seq(secondHeader))
 
         val res = compare[Array[Byte]](first, second)(Comparator.byteArray)
-        val exp = (Same(), Different())
+        val exp = (Same, Different(NonEmptyChain(Removed(firstHeader), Additional(secondHeader)))) 
         assert(res == exp)
       }
     }
-
+    test("can run analysis of requests") {
+      test("same requests are the same") {
+        type TestResponse = Response[Either[String, Array[Byte]]]
+        val candidateResponse: TestResponse = Response(Right(Array(0)), StatusCode.Ok, "OK")
+        val firstResponse: TestResponse = Response(Right(Array(0)), StatusCode.Ok, "OK")
+        val secondResponse: TestResponse = Response(Right(Array(0)), StatusCode.Ok, "OK")
+        implicit val contextShift: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
+        AsyncHttpClientCatsBackend[cats.effect.IO]().map {
+          backend =>
+            SttpBackendStub(backend)
+              .whenRequestMatches(_.uri == uri"www.candidate.com")
+              .thenRespondWrapped(_ => IO.pure(candidateResponse))
+              .whenRequestMatches(_.uri == uri"www.first.com")
+              .thenRespondWrapped(_ => IO.pure(firstResponse))
+              .whenRequestMatches(_.uri == uri"www.second.com")
+              .thenRespondWrapped(_ => IO.pure(secondResponse))
+        }.flatMap { backend: SttpBackendStub[IO, Nothing] =>
+          runAnalysis(uri"www.candidate.com", (uri"www.first.com", uri"www.second.com"),
+            List(basicRequest.response(asByteArray)))(Comparator.byteArray, backend).sequence
+        }.map {
+          assertMatch(_) { case List((Same, Same)) => }
+        }.unsafeToFuture()
+      }
+      test("different requests give a difference") {
+        type TestResponse = Response[Either[String, Array[Byte]]]
+        val additional: Byte = 3
+        val candidateResponse: TestResponse = Response(Right(Array(0,1,2,additional)), StatusCode.Ok, "OK")
+        val firstResponse: TestResponse = Response(Right(Array(0,1,2)), StatusCode.Ok, "OK")
+        val secondResponse: TestResponse = Response(Right(Array(0,1,2)), StatusCode.Ok, "OK")
+        implicit val contextShift: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
+        AsyncHttpClientCatsBackend[cats.effect.IO]().map {
+          backend =>
+            SttpBackendStub(backend)
+              .whenRequestMatches(_.uri == uri"www.candidate.com")
+              .thenRespondWrapped(_ => IO.pure(candidateResponse))
+              .whenRequestMatches(_.uri == uri"www.first.com")
+              .thenRespondWrapped(_ => IO.pure(firstResponse))
+              .whenRequestMatches(_.uri == uri"www.second.com")
+              .thenRespondWrapped(_ => IO.pure(secondResponse))
+        }.flatMap { backend: SttpBackendStub[IO, Nothing] =>
+          runAnalysis(uri"www.candidate.com", (uri"www.first.com", uri"www.second.com"),
+              List(basicRequest.response(asByteArray)))(Comparator.byteArray, backend).sequence
+        }.map { 
+          assertMatch(_) { case List((Different(Chain(Removed(additional))), Same)) => }
+        }.unsafeToFuture()
+      }
+      test("first and second response have difference in body but are the same") {
+        type TestResponse = Response[Either[String, Array[Byte]]]
+        val candidateResponse: TestResponse = Response(Right(Array(0, 0, 2)), StatusCode.Ok, "OK")
+        val firstResponse: TestResponse = Response(Right(Array(0, 0, 1)), StatusCode.Ok, "OK")
+        val secondResponse: TestResponse = Response(Right(Array(0, 0, 2)), StatusCode.Ok, "OK")
+        implicit val contextShift: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
+        AsyncHttpClientCatsBackend[cats.effect.IO]().map {
+          backend =>
+            SttpBackendStub(backend)
+              .whenRequestMatches(_.uri == uri"www.candidate.com")
+              .thenRespondWrapped(_ => IO.pure(candidateResponse))
+              .whenRequestMatches(_.uri == uri"www.first.com")
+              .thenRespondWrapped(_ => IO.pure(firstResponse))
+              .whenRequestMatches(_.uri == uri"www.second.com")
+              .thenRespondWrapped(_ => IO.pure(secondResponse))
+        }.flatMap { backend: SttpBackendStub[IO, Nothing] =>
+          runAnalysis(uri"www.candidate.com", (uri"www.first.com", uri"www.second.com"),
+              List(basicRequest.response(asByteArray)))(Comparator.byteArray, backend).sequence
+        }.map { 
+          assertMatch(_) {case List((Same, Same)) => }
+        }.unsafeToFuture()
+      }
+      test("can detect a change in the headers") {
+        type TestResponse = Response[Either[String, Array[Byte]]]
+        val candidate: TestResponse = Response(Right(Array(0,0,2)), StatusCode.Ok, "OK")
+        val base: TestResponse = Response(Right(Array(0,0,2)), StatusCode.Ok, "OK")
+        implicit val contextShift: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
+        val firstHeader = Header("X-experiment-seed", "123")
+        val secondHeader = Header("X-experiment-seed", "124")
+        val candidateResponse = candidate.copy(headers = Seq(secondHeader))
+        val firstSecondResponse = base.copy(headers = Seq(firstHeader))
+        AsyncHttpClientCatsBackend[cats.effect.IO]().map {
+          backend =>
+            SttpBackendStub(backend)
+              .whenRequestMatches(_.uri == uri"www.candidate.com")
+              .thenRespondWrapped(_ => IO.pure(candidateResponse))
+              .whenRequestMatches(_.uri == uri"www.first.com")
+              .thenRespondWrapped(_ => IO.pure(firstSecondResponse))
+              .whenRequestMatches(_.uri == uri"www.second.com")
+              .thenRespondWrapped(_ => IO.pure(firstSecondResponse))
+        }.flatMap { backend: SttpBackendStub[IO, Nothing] =>
+          runAnalysis(uri"www.candidate.com", (uri"www.first.com", uri"www.second.com"),
+              List(basicRequest.response(asByteArray)))(Comparator.byteArray, backend).sequence
+        }.map { 
+          assertMatch(_) {case List((Same,
+            Different(Chain(Removed(firstHeader), Additional(secondHeader))))) => }
+        }.unsafeToFuture()
+      }
     }
+  }
 }
